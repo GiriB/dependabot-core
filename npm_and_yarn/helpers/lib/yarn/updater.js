af639c8770c14edcb792c36df49e7cf51f851840
milind009 <milind009pincha@gmail.com>
2020-05-28 13:39:32 +0530

    Update updater.js

--- npm_and_yarn/helpers/lib/yarn/updater.js
+++ npm_and_yarn/helpers/lib/yarn/updater.js
@@ -175,7 +175,7 @@ async function updateDependencyFile(
   // requirement from the package manifest
   const replacedDeclarationYarnLock = replaceDeclaration(
     originalYarnLock,
-    readFile("yarn.lock"),
+    dedupedYarnLock,
     depName,
     newVersionRequirement,
     existingVersionRequirement
